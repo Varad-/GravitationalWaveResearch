5e76e85231709895a7c86cb7076913cafa4624c3
Varadarajan Srinivasan <varadsrinivasan4@gmail.com>
2014-07-14 18:21:26 +0200

    stuck on defining rotatez(theta):

--- PhotonGravityWave4D.py
+++ PhotonGravityWave4D.py
@@ -13,7 +13,7 @@ import matplotlib.animation as anim
 
 def TwoDimRotConj(Q,angle):
     """
-    Takes a matrix Q and returns RQR^-1 where R is the rotation matrix for the given angle. Since R is orthogonal, R^-1=R^transpose
+    Takes a 2x2 matrix Q and returns RQR^-1 where R is the 2x2 rotation matrix for the given angle. Since R is orthogonal, R^-1=R^transpose
     """
     R=np.zeros((2,2))
     R[0,0]=np.cos(angle)
@@ -22,6 +22,23 @@ def TwoDimRotConj(Q,angle):
     R[1,1]=np.cos(angle)
     return R.dot(Q).dot(R.T)
 
+def ThreeDimInvRotz(Q, angle):
+    """
+    Takes a 3x3 matrix Q and returns R^-1 Q where R is the 3x3 z-rotation matrix for the given angle. (R^-1 = R^transpose)
+    """
+    Rinv=np.zeros((3,3))
+    Rinv[0,0]=np.cos(angle)  
+    Rinv[1,1]=np.cos(angle)
+    Rinv[2,2]=1
+    Rinv[1,0]=-np.sin(angle)
+    Rinv[0,1]=np.sin(angle)
+    return Rinv.dot(Q)
+
+def rotatez(angle):
+    """
+    ??? getting nonsensical results
+    """
+    
 def delnDxSlice(tslice):
     """
     Takes a time slice and returns an evaluated 2d grid of the grid spacing (delta x) times the numerical derivative of f[row][col][time] 
@@ -84,32 +101,43 @@ means the delta t between time-slices is higher (K is prop. to (delta t)^2) so, 
 further into the future, but with a lower accuracy. Unlike evaluating an analytical solution at various times, the error from a high K 
 will grow with each time-slice because this numerical solution iteratively uses the previous 2 slices' values. Recommended K is 0.1 to 0.4.
 """
-tstepcnt=801 #tstepcnt time-slices are indexed from t=0 to t=tstepcnt-1
-rowcnt=400
-colcnt=400
+tstepcnt=401 #tstepcnt time-slices are indexed from t=0 to t=tstepcnt-1
+rowcnt=200
+colcnt=200
 
+theta = np.pi/6 #angle of incidence of grav wave
 eps=0.3 #meaning of epsilon is in the documentation
-kg=0.02 #this is the k_grav from cos(kz-kt)
-K=0.25 #K=(waveSpeed*dt/dx)**2
+kgrav=0.02 #this is the k_grav from cos(kz-kt)
+K=0.25 #K=(c*dt/dx)**2
+
+c=1 #c=1 for units of the wave speed
 
 print '\nWave equation computations of the points along the edge of the grid can be changed between taking the would-be points outside the grid as 0 (edgeType=0), or cyclically taking the corresponding boundary points on the opposite edge (edgeType=1).'
 
-print '\nThese fully adjustable parameters are currently set as:'
-print ' Number of rows = rowcnt = %d\n Number of columns = colcnt = %d\n Number of time slices = tstepcnt = %d' %(rowcnt,colcnt,tstepcnt)
-print ' waveSpeed^2 (delta t / delta x)^2 = K =',K,'   [!Read docstrings and accompanying documentation before changing K!]'
+print '\nThese are the values currently set for the fully adjustable parameters:'
+
+print '\n Computational parameters'
+print '  Number of rows: rowcnt = %d\n  Number of columns: colcnt = %d\n  Number of time slices: tstepcnt = %d' %(rowcnt,colcnt,tstepcnt)
+print '  (speed of electromagnetic wave * delta t / delta x)^2: K =',K,' [!Read docstrings and accompanying documentation before changing K!]'
+
+print '\n Physical parameters:'
+print '  Incident angle of gravitational wave: theta =',theta
+print '  In f=epsilon*cos(kz-kt),'
+print '    epsilon: eps =',eps,'\n    k: kgrav =',kgrav
+print '  Speed of electromagnetic wave: c =',c
 
 u=initialSlices(rowcnt,colcnt,tstepcnt)
 print 'Computing...'
 """
 See documentation for the derivations of the following equations and the meaning of these variable names
 """
-z=#rotate R^-1 the z of the grav wave coordinate
+M=np.zeros((2,2))
+z=rotatez(theta)#rotate R^-1 the z of the grav wave coordinate
 for t in range(2,tstepcnt):
-    f=eps*np.cos(kg*(z-t))
-    M=np.zeros((2,2))
+    f=eps*np.cos(kgrav*(z-c*t))
     M[0,0]=1+f
     M[1,1]=1-f
-    T=TwoDimRotConj(M,0)
+    T=TwoDimRotConj(M,theta)
     delsquaredLHS=delnDxSlice(T[0,0]*delnDxSlice(u[:,:,t-1])+T[0,1]*delnDySlice(u[:,:,t-1]))+delnDySlice(T[1,0]*delnDxSlice(u[:,:,t-1])+T[1,1]*delnDySlice(u[:,:,t-1]))
     u[:,:,t]=K*delsquaredLHS+2*u[:,:,t-1]-u[:,:,t-2]
 
