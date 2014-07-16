6e195baee9e603c22d2c8a32e96ab4c56f8fc849
Varadarajan Srinivasan <varadsrinivasan4@gmail.com>
2014-07-16 19:01:41 +0200

    somewhat inefficient, but working

--- PhotonGravityWave4D.py
+++ PhotonGravityWave4D.py
@@ -16,29 +16,14 @@ def TwoDimRotConj(Q,angle):
     Takes a 2x2 matrix Q and returns RQR^-1 where R is the 2x2 rotation matrix for the given angle. Since R is orthogonal, R^-1=R^transpose
     """
     R=np.zeros((2,2))
+
     R[0,0]=np.cos(angle)
     R[0,1]=-np.sin(angle)
     R[1,0]=np.sin(angle)
     R[1,1]=np.cos(angle)
+    
     return R.dot(Q).dot(R.T)
 
-def ThreeDimInvRotz(Q, angle):
-    """
-    Takes a 3x3 matrix Q and returns R^-1 Q where R is the 3x3 z-rotation matrix for the given angle. (R^-1 = R^transpose)
-    """
-    Rinv=np.zeros((3,3))
-    Rinv[0,0]=np.cos(angle)  
-    Rinv[1,1]=np.cos(angle)
-    Rinv[2,2]=1
-    Rinv[1,0]=-np.sin(angle)
-    Rinv[0,1]=np.sin(angle)
-    return Rinv.dot(Q)
-
-def rotatez(angle):
-    """
-    ??? getting nonsensical results
-    """
-    
 def delnDxSlice(tslice):
     """
     Takes a time slice and returns an evaluated 2d grid of the grid spacing (delta x) times the numerical derivative of f[row][col][time] 
@@ -101,9 +86,9 @@ means the delta t between time-slices is higher (K is prop. to (delta t)^2) so, 
 further into the future, but with a lower accuracy. Unlike evaluating an analytical solution at various times, the error from a high K 
 will grow with each time-slice because this numerical solution iteratively uses the previous 2 slices' values. Recommended K is 0.1 to 0.4.
 """
-tstepcnt=401 #tstepcnt time-slices are indexed from t=0 to t=tstepcnt-1
-rowcnt=200
-colcnt=200
+tstepcnt=151 #tstepcnt time-slices are indexed from t=0 to t=tstepcnt-1
+rowcnt=150
+colcnt=150
 
 theta = np.pi/6 #angle of incidence of grav wave
 eps=0.3 #meaning of epsilon is in the documentation
@@ -127,12 +112,26 @@ print '    epsilon: eps =',eps,'\n    k: kgrav =',kgrav
 print '  Speed of electromagnetic wave: c =',c
 
 u=initialSlices(rowcnt,colcnt,tstepcnt)
+
 print 'Computing...'
 """
 See documentation for the derivations of the following equations and the meaning of these variable names
 """
 M=np.zeros((2,2))
-z=rotatez(theta)#rotate R^-1 the z of the grav wave coordinate
+delsquaredLHS=np.zeros((rowcnt,colcnt))
+for t in range(2,tstepcnt):
+    for row in range(0,rowcnt):
+        z=-row*np.sin(theta) #this is the z inverse rotation about X. see documentation for how it becomes z=-Ysin(theta)
+        f=eps*np.cos(kgrav*(z-c*t))
+        M[0,0]=1+f
+        M[1,1]=1-f
+        T=TwoDimRotConj(M,theta)
+        delsquaredLHS[row,:]=(delnDxSlice(T[0,0]*delnDxSlice(u[:,:,t-1])+T[0,1]*delnDySlice(u[:,:,t-1]))+delnDySlice(T[1,0]*delnDxSlice(u[:,:,t-1])+T[1,1]*delnDySlice(u[:,:,t-1])))[row,:]
+    u[:,:,t]=K*delsquaredLHS+2*u[:,:,t-1]-u[:,:,t-2]
+
+"""
+M=np.zeros((2,2))
+z=0 #just set wrongly to 0
 for t in range(2,tstepcnt):
     f=eps*np.cos(kgrav*(z-c*t))
     M[0,0]=1+f
@@ -140,6 +139,7 @@ for t in range(2,tstepcnt):
     T=TwoDimRotConj(M,theta)
     delsquaredLHS=delnDxSlice(T[0,0]*delnDxSlice(u[:,:,t-1])+T[0,1]*delnDySlice(u[:,:,t-1]))+delnDySlice(T[1,0]*delnDxSlice(u[:,:,t-1])+T[1,1]*delnDySlice(u[:,:,t-1]))
     u[:,:,t]=K*delsquaredLHS+2*u[:,:,t-1]-u[:,:,t-2]
+"""
 
 """
 ------------------ numerical computations done, solutions at all times stored in u ------------------
